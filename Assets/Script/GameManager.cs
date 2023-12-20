6d3f53c38ab635badd4cada1b5135f20cb87bd26
vraus <hadrien.belleville22@gmail.com>
2023-12-20 20:26:54 +0100

    :construction: Gen Map

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -11,11 +11,40 @@ public class GameManager : MonoBehaviour
     private string map_content;
     int mapId;
 
+    [Header("Wall Infos")]
+    [SerializeField] GameObject unbreakableWallPrefab;
+    [SerializeField] GameObject breakableWallPrefab;
+    [SerializeField] GameObject target;
+    Vector3 firstItemPos = new(-3.5f, 0.5f, 3.5f);
+
     private void GetMapContent() => map_content = connectionScript.GetMapList().maps[mapId].content;
 
     private void InstantiateMap()
     {
-        foreach(char info in map_content) Debug.Log("New entry: " + info);
+        foreach(char c in map_content) Debug.Log(c);
+        int k = 0;
+        for(int i = 0; i < 8; i++)
+        {
+            Vector3 zOffset = new(0, 0, -i);
+            for (int j = 0; j < 8; j++)
+            {
+                Vector3 xOffset = new(j, 0, 0);
+                switch (map_content[k])
+                {
+                    case '*':
+                        GameObject unbreakWall = Instantiate(unbreakableWallPrefab, firstItemPos + xOffset + zOffset, Quaternion.identity);
+                        Debug.Log("Content: " + map_content[k]);
+                        break;
+                    case '=':
+                        GameObject breakWall = Instantiate(breakableWallPrefab, firstItemPos + xOffset + zOffset, Quaternion.identity);
+                        Debug.Log("Content: " + map_content[k]);
+                        break;
+                    default:
+                        break;
+                }
+                k++;
+            }
+        }
     }
 
     public void GameInitialization(int mapId)
